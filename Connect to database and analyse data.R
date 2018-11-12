f115f6a370871adb8cc47fdaba0708bfcfe87960
DaveyH1 <dave_heaton1@hotmail.com>
2018-11-12 22:34:51 +0000

    Create a connection to a database, extratc data and begin to analyse it

---
+++ Connect to database and analyse data.R
@@ -0,0 +1,41 @@
+#Connect to  SQL Server
+library(odbc)
+library(ggplot2)
+library(dplyr)
+
+con <- DBI::dbConnect(odbc::odbc(), Driver = "SQL Server", Server = "DAVE-PC", 
+                      Database = "NYCTaxi_Sample", Trusted_Connection = "True")
+
+#extract some data
+df <- dbGetQuery(con, "SELECT TOP 5000
+                        	medallion,
+                        	fare_amount,
+                        	trip_distance
+                        
+                        FROM
+                        	dbo.nyctaxi_sample
+                        
+                        TABLESAMPLE(5000 ROWS)")
+
+#View the record
+View(df)
+
+summary(df)
+
+#Plot
+ggplot(df, aes(x = trip_distance, y = fare_amount)) +
+  geom_point(alpha = 0.5) + 
+  geom_smooth(method = "lm") +
+  xlab("Distance") +
+  ylab("Fare")
+
+ggplot(df, aes(x = trip_distance, y = log10(fare_amount))) +
+  geom_point(alpha = 0.5) + 
+  xlab("Distance") +
+  ylab("Fare")
+
+#Check linear regression
+summary(lm(df$fare_amount ~ df$trip_distance))
+
+summary(df$trip_distance)
+summary(df$fare_amount)
